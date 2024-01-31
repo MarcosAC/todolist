ce110220fb359b4b2dcb4ebf91cafca1c23d5074
Marcos Aurelio <marcos.aucorrea@gmail.com>
2024-01-30 22:34:15 -0300

    first commit

--- lib/screens/task_form.dart
+++ lib/screens/task_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todolist/models/task.dart';
 
 class TaskForm extends StatefulWidget {
   const TaskForm({super.key});
@@ -10,6 +11,18 @@ class TaskForm extends StatefulWidget {
 class _TaskFormState extends State<TaskForm> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Adicionar Tarefa"),
+        titleTextStyle: const TextStyle(fontSize: 17),
+        actions: [
+          IconButton(onPressed: () {
+            Task newTask = Task(title: title, time: time)
+          }, icon: const Icon(Icons.save)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.list)),
+        ],        
+      ),
+      body: const Padding(padding: EdgeInsets.all(8.0)),
+    );
   }
 }
